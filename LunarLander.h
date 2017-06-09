99f6984140528b00e082fb1a8013b39651fdf2fd
Christoph LEHR <m1525189@ti33.tilab.tuwien.ac.at>
2017-06-09 10:30:27 +0200

    added .h for global definitions

---
+++ LunarLander.h
@@ -0,0 +1,9 @@
+//Interface TerrainGernerator
+#define TERRAIN_POINTS 17
+#define MAXIMUM_HEIGHT 64
+#define MAXIMUM_HEIGHT_PLAIN 16
+#define ZOOM_OUT_DISTANCE 256
+#define ZOOM_IN_DISTANCE  128
+
+//Interface PS
+#define PS2CharArraySize 67
