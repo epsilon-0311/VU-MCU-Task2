1e4935af575e572ce67b0aaa508b2e23e28855e5
Christoph LEHR <m01525189@ti33.tilab.tuwien.ac.at>
2018-12-22 17:40:53 +0100

    updated udp config

--- udp_config.h
+++ udp_config.h
@@ -5,17 +5,17 @@
 
 #define UDP_PORT	50000UL
 // note the ',' (instead of the usual '.') between numbers
-#define DESTINATION	10,60,0,1
+#define DESTINATION	192,168,42,1
 
 // the following settings are only applied if CUSTOM_IP_SETTINGS is defined
 // note the ',' (instead of the usual '.') between numbers
-#define IP		10,60,0,10
+#define IP		192,168,42,2
 #define NETMASK	255,255,255,0
-#define GATEWAY	10,60,0,1
+#define GATEWAY	192,168,42,1
 
 // Memory Pool Settings
 #define MAX_MSG_LEN	128
-#define MSG_POOL_SIZE 128
+#define MSG_POOL_SIZE 16
 
 typedef struct udp_msg {
 	uint8_t data[MAX_MSG_LEN];
